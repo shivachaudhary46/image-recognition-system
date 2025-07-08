71e8dfcf537c3044c10bbb110450c40d611bd8cd
shivachaudhary46 <shivachaudhary4116@gmail.com>
2025-07-08 18:47:36 +0545

    image recognition system of pet classifier

--- app/app.py
+++ app/app.py
@@ -1,10 +1,32 @@
-from flask import Flask 
+from flask import Flask, request, render_template, url_for
+from keras.preprocessing import image
+from keras.models import load_model
+import numpy as np
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello World"
+# load your trained model (you need to provide the correct)
+cnn = load_model('../model.keras')
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+@app.route('/', methods=['GET', 'POST'])
+def predict():
+    '''gives us prediction by evaluating model'''
+    prediction = None 
+    if request.method == 'POST':
+        img_file = request.files['img']
+        if img_file:
+            img_path = 'temp.img'
+            img_file.save(img_path)
+            test_image = image.load_img(img_path, target_size=(64, 64))
+            test_image = image.img_to_array(test_image)
+            test_image = np.expand_dims(test_image, axis=0)
+            result = cnn.predict(test_image)
+            if result[0][0] == 1:
+                prediction = 'dog'
+            else:
+                prediction = 'cat'
+
+    return render_template('home.html', prediction = prediction)
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- test_app.py
+++ test_app.py
@@ -1,13 +1,3 @@
-import sys
-import os 
+filename = "neo.jpg"
 
-# Add the app directory to the system path
-sys.path.insert(0, os.path.abspath("app"))
-
-from app import app 
-
-## importing function 
-def test_home():
-    response=app.test_client().get("/")
-    assert response.status_code==200
-    assert response.data==b"Hello World"
+print(filename.rsplit('.', 1)[1].lower())
